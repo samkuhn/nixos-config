f4126a0238630fd564c08a72d75298aa4df2a0a8
Sam Kuhn <sam@samkuhn.dev>
2024-03-24 20:19:07 +0000

    enable x / kde

--- system/configuration.nix
+++ system/configuration.nix
@@ -119,7 +119,7 @@
   #};
 
   networking = {
-    hostName = "roxstrixg1660ti";
+    hostName = "rogstrixg1660ti";
 
     #firewall = {
     #  enable = true;
@@ -153,6 +153,45 @@
   #    noto-fonts-emoji
   #  ];
   #};
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "gb";
+    xkbVariant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "uk";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
 
   environment.systemPackages = with pkgs; [
     bash
