3e8bd14a45dad753253a9f1748b1a330fd923e29
Sam Kuhn <sam@samkuhn.dev>
2024-03-24 21:19:01 +0000

    remove dup

--- system/configuration.nix
+++ system/configuration.nix
@@ -107,14 +107,6 @@
     driSupport32Bit = true;
   };
 
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-  };
-
   networking = {
     hostName = "rogstrixg1660ti";
 
