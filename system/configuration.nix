19490bcf84646470f0f1b1b7b0b0bb89a34414fc
Sam Kuhn <sam@samkuhn.dev>
2024-03-24 22:03:57 +0000

    nvidia, wifi, overreaching

--- system/configuration.nix
+++ system/configuration.nix
@@ -93,10 +93,10 @@
     };
   };
 
-  #hardware.logitech.wireless = {
-  #  enable = true;
-  #  enableGraphical = true;
-  #};
+  hardware.logitech.wireless = {
+    enable = true;
+    enableGraphical = true;
+  };
 
   # Workaround: https://github.com/NixOS/nixpkgs/issues/114222
   #systemd.user.services.telephony_client.enable = false;
@@ -105,6 +105,53 @@
     enable = true;
     driSupport = true;
     driSupport32Bit = true;
+  };
+
+  # SK Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"]; # or "nvidiaLegacy470 etc.
+
+  # SK nvidia driver
+  hardware.nvidia = {
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  # SK nvidia prime
+  hardware.nvidia.prime = {
+    offload = {
+      enable = true;
+      enableOffloadCmd = true;
+    };
+    #sync.enable = true;
+    # Make sure to use the correct Bus ID values for your system!
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
   };
 
   networking = {
@@ -197,13 +244,61 @@
     iw
     wirelesstools # iwconfig
 
-    docker
+    # docker
 
     usbutils # lsusb
 
     #polkit_gnome
 
     sbctl
+
+    neovim 
+    #lunarvim
+    tmux 
+    wget
+    git
+    #nodejs
+    #yarn
+    google-chrome
+    python3
+    htop
+    btop
+    nvtop
+    glmark2
+    inkscape-with-extensions
+    pinta
+    lshw
+    #direnv
+    #vscode
+    (vscode-with-extensions.override {
+      vscodeExtensions = with vscode-extensions;
+        [
+          bbenoist.nix
+          ms-python.python
+          ms-azuretools.vscode-docker
+          ms-vscode-remote.remote-ssh
+        ]
+        ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+          #{
+          #  name = "Supermaven";
+          #  publisher = "Supermaven";
+          #  version = "0.1.19";
+          #  sha256 = "7e7f5c54f7d15c8b9bc7b2029d0135ba1de2adb0cf273f65c08dadc6b8a0eea0";
+          #}
+          {
+            name = "cody-ai";
+            publisher = "sourcegraph";
+            version = "1.9.1710429091";
+            sha256 = "eaa298a4d63bd018b049fb0b340053778d1a97c3639b277f179400a9e8bc2574";
+          }
+          #{
+          #  name = "gpt-pilot-vs-code";
+          #  publisher = "PythagoraTechnologies";
+          #  version = "0.1.5";
+          #  sha256 = "8565e0f40735a16c5930409242ec100ea549038496db57836cb2fb6d8efb97cb";
+          #}
+        ];
+    })
   ];
 
   # Use experimental nsncd. See https://flokli.de/posts/2022-11-18-nsncd/
