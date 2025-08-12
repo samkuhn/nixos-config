a912400ea7f2af17c4921243343910b21ab7fc45
Sam Kuhn <sam@samkuhn.dev>
2025-08-12 09:23:33 +0000

    disable nvidia drivers abroad reinstall issue

--- system/configuration.nix
+++ system/configuration.nix
@@ -118,22 +118,22 @@
   };
 
   # SK Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"]; # or "nvidiaLegacy470 etc.
+  #services.xserver.videoDrivers = ["nvidia"]; # or "nvidiaLegacy470 etc.
 
   # SK nvidia driver
-  hardware.nvidia = {
+  #hardware.nvidia = {
     # Modesetting is required.
-    modesetting.enable = true;
+  #  modesetting.enable = true;
 
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
     # Enable this if you have graphical corruption issues or application crashes after waking
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
     # of just the bare essentials.
-    powerManagement.enable = false;
+  #  powerManagement.enable = false;
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
+  #  powerManagement.finegrained = false;
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
@@ -142,27 +142,27 @@
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = true;
+  #  open = true;
 
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
+  #  nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
+  #  package = config.boot.kernelPackages.nvidiaPackages.stable;
+  #};
 
   # SK nvidia prime
-  hardware.nvidia.prime = {
-    offload = {
-      enable = true;
-      enableOffloadCmd = true;
-    };
-    #sync.enable = true;
-    # Make sure to use the correct Bus ID values for your system!
-    intelBusId = "PCI:0:2:0";
-    nvidiaBusId = "PCI:1:0:0";
-  };
+#  hardware.nvidia.prime = {
+#    offload = {
+#      enable = true;
+#      enableOffloadCmd = true;
+#    };
+#    #sync.enable = true;
+#    # Make sure to use the correct Bus ID values for your system!
+#    intelBusId = "PCI:0:2:0";
+#    nvidiaBusId = "PCI:1:0:0";
+#  };
 
   networking = {
     hostName = "rogstrixg1660ti";
